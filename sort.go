afd84f0ec89418183b6d4f3fefef64a7b99acb80
geneCh <hit_zhangjian@163.com>
2021-03-04 20:09:55 +0800

    insert sort

--- sort.go
+++ sort.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // 排序的稳定性: 相同的数据, 原先的前后顺序在排序后没有改变则为稳定的, 改变了则为不稳定的
 
-// 冒泡排序:
+// 冒泡排序: -- 稳定排序
 // 1. 外层控制行
 // 2. 内层控制列
 // 3. 比较相邻元素
@@ -45,11 +45,38 @@ func BubbleSortAdvance(arr []int) {
 	fmt.Println("比较了:", count)
 }
 
+// 选择排序 -- 不稳定排序
+// 1. 在元素中找到最大值的下标
+// 2. 跟剩余值中最后一个元素交换位置
+func SelectSort(arr []int) {
+	var index, count int
+	for i := 0; i < len(arr)-1; i++ {
+		index = 0 // 每次都用第一个值进行初始化
+		for j := 1; j < len(arr)-i; j++ { // 所以j从下标1开始, 因为初始化是0无需在比较了
+			count++
+			if arr[j] > arr[index] {
+				// 记录最大值下标
+				index = j
+			}
+		}
+		// 交换最大值到最后位置
+		arr[index], arr[len(arr)-1-i] = arr[len(arr)-1-i], arr[index]
+	}
+	fmt.Println("比较了:", count)
+}
+
+// 插入排序 --
+// 算法设计的思路是，将数组划分成两部分，第一部分是有序的，第二部分是无序的
+// 每次从无序部分取一个元素，将这个元素插入到有序部分，保持有序部分的有序性质
+// 直到无序部分为空
+
 func OutCall_st() {
 	arr := []int{1, 3, 5, 2, 8, 6, 4, 9}
-	arr1 := []int{1, 2, 3, 4, 5, 8, 6, 9}
-	BubbleSort(arr)
+	//arr1 := []int{1, 2, 3, 4, 5, 8, 6, 9}
+	//BubbleSort(arr)
+	//fmt.Println(arr)
+	//BubbleSortAdvance(arr1)
+	//fmt.Println(arr1)
+	SelectSort(arr)
 	fmt.Println(arr)
-	BubbleSortAdvance(arr1)
-	fmt.Println(arr1)
 }
